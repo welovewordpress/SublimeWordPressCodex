f6b01b089cc21f66b3cd09ae4c60e0cecd984bc7
Tom J Nowell <contact@tomjn.com>
2013-02-18 16:43:33 +0000

    Added support for Queryposts.com and changed the word ordering to make more sense for searches

--- WordPressCodex.py
+++ WordPressCodex.py
@@ -16,8 +16,16 @@ def SearchWpCodexFor(text):
     url = 'http://wordpress.org/search/' + text.replace(' ','%20')
     webbrowser.open_new_tab(url)
 
+def SearchQPFor(text):
+    url = 'http://queryposts.com/?s=' + text.replace(' ','%20')
+    webbrowser.open_new_tab(url)
+
 def OpenWpFunctionReference(text):
     url = 'http://codex.wordpress.org/Function_Reference/' + text.replace(' ','%20')
+    webbrowser.open_new_tab(url)
+
+def OpenQPFunctionReference(text):
+    url = 'http://queryposts.com/function/' + text.replace(' ','%20')
     webbrowser.open_new_tab(url)
 
 class WordpressCodexOpenSelectionCommand(sublime_plugin.TextCommand):
@@ -47,6 +55,40 @@ class WordpressCodexSearchFromInputCommand(sublime_plugin.WindowCommand):
             self.on_done, self.on_change, self.on_cancel)
     def on_done(self, input):
         SearchWpCodexFor(input)
+
+    def on_change(self, input):
+        pass
+
+    def on_cancel(self):
+        pass
+# query_posts_search_selection
+class QueryPostsOpenSelectionCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for selection in self.view.sel():
+            # if the user didn't select anything, search the currently highlighted word
+            if selection.empty():
+                selection = self.view.word(selection)
+
+            text = self.view.substr(selection)
+            OpenQPFunctionReference(text)
+
+class QueryPostsSearchSelectionCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for selection in self.view.sel():
+            # if the user didn't select anything, search the currently highlighted word
+            if selection.empty():
+                selection = self.view.word(selection)
+
+            text = self.view.substr(selection)            
+            SearchQPFor(text)
+
+class QueryPostsSearchFromInputCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        # Get the search item
+        self.window.show_input_panel('Search QueryPosts.com for', '',
+            self.on_done, self.on_change, self.on_cancel)
+    def on_done(self, input):
+        SearchQPFor(input)
 
     def on_change(self, input):
         pass
